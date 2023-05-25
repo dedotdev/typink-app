1a22feff154de438fad4fef18b5a6aca0dbf04bb
Thang X. Vu <thang@coongcrafts.io>
2023-05-25 15:24:49 +0700

    refactor: format balance

--- src/components/AccountBalances.tsx
+++ src/components/AccountBalances.tsx
@@ -2,6 +2,7 @@ import { Flex, Skeleton, Stack, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useAsync, useBoolean } from 'react-use';
 import { FrameSystemAccountInfo } from '@polkadot/types/lookup';
+import { formatBalance } from '@polkadot/util';
 import { useApiContext } from '@/providers/ApiProvider';
 
 interface AccountBalancesProps {
@@ -9,7 +10,7 @@ interface AccountBalancesProps {
 }
 
 export default function AccountBalances({ address }: AccountBalancesProps) {
-  const { api } = useApiContext();
+  const { api, network, apiReady } = useApiContext();
   const [loading, setLoading] = useBoolean(true);
   const [balance, setBalance] = useState<FrameSystemAccountInfo>();
 
@@ -30,15 +31,15 @@ export default function AccountBalances({ address }: AccountBalancesProps) {
   const values = [
     {
       label: 'Free Balance',
-      amount: balance?.data?.free?.toHuman() || 0,
+      amount: balance?.data?.free || 0,
     },
     {
       label: 'Reserved Balance',
-      amount: balance?.data?.reserved?.toHuman() || 0,
+      amount: balance?.data?.reserved || 0,
     },
     {
       label: 'Frozen Balance',
-      amount: balance?.data?.frozen?.toHuman() || 0,
+      amount: balance?.data?.frozen || 0,
     },
   ];
 
@@ -47,8 +48,15 @@ export default function AccountBalances({ address }: AccountBalancesProps) {
       {values.map(({ label, amount }) => (
         <Flex key={label} gap={2}>
           <Text>{label}:</Text>
-          <Skeleton h={6} minW={10} isLoaded={!loading}>
-            <strong>{amount}</strong>
+          <Skeleton h={6} minW={10} isLoaded={apiReady && !loading}>
+            <strong>
+              {formatBalance(amount, {
+                decimals: network.decimals,
+                withUnit: false,
+              })}
+              &nbsp;
+              {network.symbol}
+            </strong>
           </Skeleton>
         </Flex>
       ))}
