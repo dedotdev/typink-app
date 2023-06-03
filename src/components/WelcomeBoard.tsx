ca0a97c9cb52e8e6163ae113291144b2f7674567
Thang X. Vu <thang@coongcrafts.io>
2023-06-03 17:18:58 +0700

    refactor: update headline

--- src/components/WelcomeBoard.tsx
+++ src/components/WelcomeBoard.tsx
@@ -5,7 +5,7 @@ export default function WelcomeBoard() {
   return (
     <Box textAlign='center'>
       <Heading as='h1' fontSize='3xl' mb={6} mt={16}>
-        <small>Welcome to</small> <br /> Coong Dapp Playground
+        <small>Welcome to</small> <br /> Coong Playground Dapp
       </Heading>
       <Text mb={4} fontSize='lg'>
         Connect to your wallet to getting started
