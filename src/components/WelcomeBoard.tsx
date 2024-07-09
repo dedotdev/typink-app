9ebc4286806ddabffeba4c42e7a180a2e757adbc
Thang X. Vu <thang@coongcrafts.io>
2024-07-09 17:06:05 +0700

    text tweaks

--- src/components/WelcomeBoard.tsx
+++ src/components/WelcomeBoard.tsx
@@ -8,9 +8,15 @@ export default function WelcomeBoard() {
       <Box w={28} mx='auto' mt={8} mb={4}>
         <img src='/dedot-dark-logo.png'/>
       </Box>
-      <Heading mb={6} size='lg'>
+      <Text as='h1'
+            bgGradient='linear(to-l, #7928CA, #FF0080)'
+            bgClip='text'
+            fontSize='3xl'
+            fontWeight='bold'
+            mb={4}
+      >
         Welcome to Dedot Demo Dapp
-      </Heading>
+      </Text>
       <Text mb={4} fontSize='xl'>
         Connect to your wallet to getting started
       </Text>
