b7c3aed6b795882366ca0e1e8ce9d6aeeab502ff
Thang X. Vu <thang@coongcrafts.io>
2024-08-16 00:00:05 +0700

    add toast connecting to network

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { useAsync, useLocalStorage, useToggle } from 'react-use';
 import { NetworkInfo } from '@/types';
 import { DedotClient, JsonRpcProvider, LegacyClient, WsProvider } from 'dedot';
@@ -33,6 +34,10 @@ export default function useApi(network?: NetworkInfo): UseApi {
 
     let provider: JsonRpcProvider;
 
+    // TODO might be not a good idea to put the toast here,
+    //  but it's okay for now for demo purposes!
+    const toastId = toast.info(`Connecting to ${network.name}`, { autoClose: false, isLoading: true });
+
     provider = new WsProvider(network.provider);
 
     setApi(await DedotClient.new({ provider, cacheMetadata }));
@@ -41,6 +46,13 @@ export default function useApi(network?: NetworkInfo): UseApi {
     // setApi(undefined);
 
     setReady(true);
+
+    toast.update(toastId, {
+      render: `Connected to ${network.name}`,
+      autoClose: 2000,
+      isLoading: false,
+      type: 'success',
+    });
   }, [network?.provider]);
 
   return { ready, api, legacy };
