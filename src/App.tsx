3fa7545249710342941a0864db823503b41d8fb6
Thang X. Vu <thang@coongcrafts.io>
2024-08-24 15:40:41 +0700

    handle tab index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Box, Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useSearchParam } from 'react-use';
 import GreetBoard from '@/components/GreeterBoard.tsx';
 import Psp22Board from '@/components/Psp22Board.tsx';
 import BalanceInsufficientAlert from '@/components/shared/BalanceInsufficientAlert.tsx';
@@ -6,12 +8,21 @@ import MainFooter from '@/components/shared/MainFooter';
 import MainHeader from '@/components/shared/MainHeader';
 
 function App() {
+  const tab = useSearchParam('tab');
+  const tabIndex = tab ? parseInt(tab) : 0;
+  const [index, setIndex] = useState(tabIndex);
+
+  const handleTabsChange = (index: number) => {
+    setIndex(index);
+    history.pushState({}, '', location.pathname + `?tab=${index}`);
+  };
+
   return (
     <Flex direction='column' minHeight='100vh'>
       <MainHeader />
       <Box maxWidth='container.md' mx='auto' my={4} px={4} flex={1} w='full'>
         <BalanceInsufficientAlert />
-        <Tabs>
+        <Tabs index={index} onChange={handleTabsChange}>
           <TabList>
             <Tab>Greeter Contract</Tab>
             <Tab>PSP22 Contract</Tab>
